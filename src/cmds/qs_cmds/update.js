d2bc700b0bc993d77465051012c0af6d4ded6893
Frito <fritogotlayed@gmail.com>
2019-10-17 21:57:55 -0500

    Added support for queue details and update

---
+++ src/cmds/qs_cmds/update.js
@@ -0,0 +1,48 @@
+const got = require('got');
+const urlJoin = require('url-join');
+
+const utils = require('../../../lib/utils');
+
+const updateQueue = ({ queue, resource, env }) => utils.getEnvConfig(env)
+  .then((conf) => urlJoin(conf.qsUrl, 'queue', queue))
+  .then((url) => {
+    const body = { };
+
+    if (resource) {
+      body.resource = resource.toUpperCase() === 'NULL' ? null : resource;
+    }
+
+    const postOptions = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      throwHttpErrors: false,
+      body: JSON.stringify(body),
+    };
+
+    return got.post(url, postOptions);
+  });
+
+const printResult = (statusCode) => {
+  if (statusCode === 200) {
+    utils.display('Queue updated successfully.');
+  } else {
+    utils.display('An error occurred while updating the queue.');
+    utils.display(`Status: ${statusCode}`);
+  }
+};
+
+exports.command = 'update <queue>';
+exports.desc = 'Updates metadata around the <queue> queue.';
+exports.builder = {
+  env: {
+    default: 'default',
+    desc: 'The environment to write this value to',
+  },
+  resource: {
+    default: null,
+    desc: 'resource to be invoked upon message being enqueued. Use the string "null" to delete the current value',
+  },
+};
+exports.handler = (argv) => updateQueue(argv)
+  .then((resp) => printResult(resp.statusCode));
