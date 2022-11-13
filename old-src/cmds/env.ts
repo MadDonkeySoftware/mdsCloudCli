d24e9b18a43e7cb169901cc0b1f23a0503376106
Frito <fritogotlayed@gmail.com>
2022-11-13 10:42:38 -0600

    removing old-src

--- old-src/cmds/env.ts
+++
@@ -1,5 +0,0 @@
-import { Argv } from 'yargs';
-
-export const command = 'env <command>';
-export const describe = 'Inspect or adjust mds CLI environments.';
-export const builder = (yargs: Argv) => yargs.commandDir('env_cmds');
--- old-src/cmds/fs.ts
+++
@@ -1,5 +0,0 @@
-import { Argv } from 'yargs';
-
-export const command = 'fs <command>';
-export const describe = 'Interface with the file service';
-export const builder = (yargs: Argv) => yargs.commandDir('fs_cmds');
